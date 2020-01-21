1deaa4d743f31664e4baab1f93164e8256a190d7
Iain <iain2eyes@gmail.com>
2020-01-21 10:02:17 +0000

    Employee set up

---
+++ src/main/java/com/codeclan/example/relationshipslab/RelationshipslabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.relationshipslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipslabApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codeclan/example/relationshipslab/models/Project.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.relationshipslab.models;
+
+public class Project {
+}
